4e6aad24c234d22fb4c7846e2b0ea0b9385b68fa
sannijesufemi <sannijesufemi@gmail.com>
2022-09-08 16:32:12 -0700

    Task 8: UNIX OS

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,9 +8,9 @@
 *
 *Return: This will return (1) as error
 */
-int main(void) 
+int main(void)
 {
-	char quotes[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char quotes[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	
 	write(1, quotes, 59);
 	return (1);
