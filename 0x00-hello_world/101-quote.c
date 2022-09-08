0363a0ac78caa6c1feef419854f820f827283162
sannijesufemi <sannijesufemi@gmail.com>
2022-09-08 16:34:26 -0700

    Task 8: fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,8 +10,7 @@
 */
 int main(void)
 {
-	char quotes[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
+	char quotes[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(1, quotes, 59);
 	return (1);
 }
