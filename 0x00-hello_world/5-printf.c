3242cb0e0f9c814e9a3ab2f830965697cf1e46a6
sannijesufemi <sannijesufemi@gmail.com>
2022-09-08 14:16:22 -0700

    Task 5: Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This print using the printf function
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
