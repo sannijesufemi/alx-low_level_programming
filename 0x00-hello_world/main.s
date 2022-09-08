aeb792c38192560ad56720e12199e26214cce4d8
sannijesufemi <sannijesufemi@gmail.com>
2022-09-08 15:47:45 -0700

    Task 7: Intel

--- 0x00-hello_world/main.s
+++ 0x00-hello_world/main.s
@@ -1,5 +1,24 @@
 	.file	"main.c"
+	.intel_syntax noprefix
 	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	mov	eax, 0
+	pop	rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
 	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
 	.section	.note.GNU-stack,"",@progbits
 	.section	.note.gnu.property,"a"
