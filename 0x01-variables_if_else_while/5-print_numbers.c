88f817ff5e197d0c40ad6e87b2a2a06b019b76d8
sannijesufemi <sannijesufemi@gmail.com>
2022-09-09 10:29:49 -0700

    Task 5: Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This print numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i = 0;
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+
+}
