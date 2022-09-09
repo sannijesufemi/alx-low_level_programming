783c9191ea7c5699bb2c21de772dd3392a7a4a7c
sannijesufemi <sannijesufemi@gmail.com>
2022-09-09 15:38:41 -0700

    Task 2: Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -11,12 +11,11 @@
 int main(void)
 {
 	int num = 0;
-	char i;
 
 	while (num <= 9)
 	{
-		i = (int)num;
-		putchar(i);
+		/*This converts the value to ASCII*/
+		putchar(num + '0');
 		++num;
 	}
 	putchar('\n');
