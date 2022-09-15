bd5fb151fdea8f7c5c997614c301cc96b267c6c3
sannijesufemi <sannijesufemi@gmail.com>
2022-09-15 15:44:14 -0700

    Task 2: mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include"main"
+
+/**
+ * mul - computes multiplication of two numbers
+ * @a: first input
+ * @b: second input
+ * Return: mult
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
