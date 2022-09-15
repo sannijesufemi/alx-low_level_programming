58b5d67a1c5f8902caee8cd97d1e7c67fd95eaca
sannijesufemi <sannijesufemi@gmail.com>
2022-09-15 07:35:32 -0700

    Taks 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * main - tests if a number is positive or negative
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 98 % 2;
+	positive_or_negative(i);
+
+	return (0);
+}
+
