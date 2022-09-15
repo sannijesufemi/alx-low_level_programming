3561ad1def095d442fe9702d7c32c730d05c488c
sannijesufemi <sannijesufemi@gmail.com>
2022-09-15 16:28:29 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_most_numbers - prints 0 to 9
- * 		excluding 2 and 4 using _putchar twice
+ *	 excluding 2 and 4 using _putchar twice
  * Return: Always 0 (Success)
 */
 
@@ -10,7 +10,7 @@ void print_most_numbers(void)
 {
 	int num;
 
-	for (num = 0; num <=9; ++num)
+	for (num = 0; num <= 9; ++num)
 	{
 		if (num == 2 || num == 4)
 			continue;
