d3981698e940f9ec3cede2aa31106fa599b48168
sannijesufemi <sannijesufemi@gmail.com>
2022-09-15 15:29:11 -0700

    Task isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include"main"
+
+/**
+ * _isdigit - Checks if a character is a digit or not
+ * @c: input to check
+ *
+ * Return: 1 if its a digit, 0 if otherwise
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (2);
+}
