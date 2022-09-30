c2e832d0dd2c2e4bff09ec99a434e469422d5188
sannijesufemi <sannijesufemi@gmail.com>
2022-09-30 10:28:58 -0700

    Task 1

---
+++ 0x09-static_libraries/create_static_lib.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# a script that creates a static library
+# called libmy.a from all the .c files
+# that are in the current directory
+
+gcc -c *.c
+ar rc libmy.a *.o
+echo y | rm -I *.o
+
