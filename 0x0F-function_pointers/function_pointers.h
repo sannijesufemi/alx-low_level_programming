4ea37071beae6c78c4fff5e55f2d7c1c17cd931a
sannijesufemi <sannijesufemi@gmail.com>
2022-10-12 08:46:29 -0700

    Task 3

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -6,5 +6,16 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int _putchar(char c);
 
+/**
+ * enum bool - typedef function for bool
+ * @TRUE: 1
+ * @FALSE: 0
+*/
+typedef enum bool
+{
+	TRUE = 1,
+	FALSE = 0
+} bool;
+
 #endif /* FUNCTION_POINTERS_H */
 
